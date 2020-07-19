d7132cb2f2d2c4991b2a007513217c85b4a9fcff
githubprabh <prabhnoor1998@gmail.com>
2020-07-20 01:35:29 +0530

    Create HeightBinaryTree.java

---
+++ TasksFrom2015Indeed/HeightBinaryTree.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(Tree T) {
+        // write your code in Java SE 8
+        return height(T);
+    }
+
+    // recursively get the height of the binary tree
+    public int height(Tree t) {
+        // tree t is empty
+        if (t == null)
+            return -1;
+        // get the maximum of the height of the left tree and the right tree,
+        // and return the value plus 1 because of counting the current node.
+        else
+            return Math.max(height(t.l) + 1, height(t.r) + 1);
+    }
+}
