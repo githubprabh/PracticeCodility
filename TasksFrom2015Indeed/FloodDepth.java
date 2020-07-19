99a090dc7df131dda4682e47af6b1a1de4630afd
githubprabh <prabhnoor1998@gmail.com>
2020-07-20 01:23:15 +0530

    Create FloodDepth.java

---
+++ TasksFrom2015Indeed/FloodDepth.java
@@ -0,0 +1,25 @@
+
+public class FloodDepth {
+
+    public int getMaximumDepth(int[] A) {
+        int highestIdx = 0;
+        int lowestIdx = 0;
+        int max = 0;
+
+        for (int i = 1; i < A.length; i++) {
+            int current = A[i];
+            int highest = A[highestIdx];
+            int lowest = A[lowestIdx];
+            if (current > highest) {
+                max = Math.max(highest - lowest, max);
+                highestIdx = i;
+                lowestIdx = highestIdx;
+            } else if (current > lowest) {
+                max = Math.max(current - lowest, max);
+            } else if (current < lowest) {
+                lowestIdx = i;
+            }
+        }
+        return max;
+    }
+}
